8dd1d1fa8a2038bc88cd8ddb0924556b956bf201
ayushman-c <ayushmanc67@gmail.com>
2025-10-11 11:56:27 +0530

    Coming soon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,78 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+   
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="logo-container">
+          <div className="logo"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+        <div className="navigation-icons">
+          <div className="about">About</div>
+          <div className="features">Features</div>
+          <div className="tech-stack">Tech Stack</div>
+          <div className="benefits">Benefits</div>
+          <div className="get-notified">Get Notified</div>
+        </div>
+      </div>
     </div>
+      
+    
+<div className="hero-section">
+   
+    <div className="sectionA">    
+      <div className="launching-soon-container">
+        <div className="spark"></div>
+        <div className="text">Launching Soon</div>
+      </div>
+    
+    <div className="coming-soon-text">Coming Soon</div>
+    <div className="work-track-grow">Work, Track, Grow</div>  
+    <div className="description">Business Analytics simplified for small businesses using AI. Munsi AI is your intelligent companion for smarter business decisions.</div>
+    <div className="CTA">
+    <div className="learn-more-button">
+      <div className="text">Learn More</div>
+      <div className="downarrow"></div>
+    </div>
+    <div className="early-access">Get Early Access</div>
+    </div>
+    <div className="minidetails">
+      <div className="minidetails-1">
+        <div className="main-text-1">AI-Powered</div>
+        <div className="sub-text-1">Analytics</div>
+      </div>
+      <div className="minidetails-2">
+        <div className="main-text-2">SMBs</div>
+        <div className="sub-text-2">Focused</div>
+      </div>
+      <div className="minidetails-3">
+        <div className="main-text-3">24/7</div>
+        <div className="sub-text-3">Support</div>
+      </div>
+    </div>
+    
+    </div> 
+
+
+
+
+    <div className="sectionB">
+        <div className="B-container">
+          <div className="img-1-container">
+            <div className="img-1"></div>
+          </div>
+          <div className="img-2-container">
+            <div className="img-2"></div>
+          </div>
+          <div className="img-3-container">
+            <div className="img-3"></div>
+          </div>
+        </div>
+    </div>
+
+</div>
+   </>
   );
 }
